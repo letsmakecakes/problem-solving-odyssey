afe27adf4faaa4fb16cefe02392bb27b7cbe4aac
Adwaith Rajeev <41227654+letsmakecakes@users.noreply.github.com>
2023-10-20 00:16:27 +0530

    Completed to determine if brackets are bakanced or not.

---
+++ determine_if_brackets_are_balanced.py
@@ -0,0 +1,49 @@
+from stack import Stack
+
+
+def is_match(p1, p2):
+    if p1 == '(' and p2 == ')':
+        return True
+    elif p1 == '[' and p2 == ']':
+        return True
+    elif p1 == '{' and p2 == '}':
+        return True
+    else:
+        return False
+
+
+def is_paren_balanced(parenthesis):
+    s = Stack()
+    is_balanced = True
+    index = 0
+
+    while index < len(parenthesis) and is_balanced:
+        paren = parenthesis[index]
+
+        if paren in "([{":
+            s.push(paren)
+        else:
+            if s.is_empty():
+                is_balanced = False
+                break
+            else:
+                top = s.pop()
+                if not is_match(top, paren):
+                    is_balanced = False
+                    break
+        index += 1
+
+    if s.is_empty() and is_balanced:
+        return True
+    else:
+        return False
+
+
+print("String : (((({})))) Balanced or not?")
+print(is_paren_balanced("(((({}))))"))
+
+print("String : [][]]] Balanced or not?")
+print(is_paren_balanced("[][]]]"))
+
+print("String : [][] Balanced or not?")
+print(is_paren_balanced("[][]"))
