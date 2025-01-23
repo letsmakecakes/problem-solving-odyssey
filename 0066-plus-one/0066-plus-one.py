9af1135b74b4a12c3049caebc3a2a853bf07caf1
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-01-23 20:21:30 +0530

    Time: 0 ms (100%), Space: 17.9 MB (5.6%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,13 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        n = len(digits)
+
+        # Traverse the array of digits from the end
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        # If all digits were 9, we need to add a leading 1
+        return [1] + digits
