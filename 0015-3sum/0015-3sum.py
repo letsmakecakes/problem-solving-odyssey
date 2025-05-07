6ed5a64fff1545e84811a77fbef23867d3eb06b4
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-05-07 20:13:15 +0530

    Time: 446 ms (88.6%), Space: 20.7 MB (69.22%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,38 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        result = set()
+        results = []
         nums.sort()
-        for index, num in enumerate(nums):
-            left = index + 1
-            right = len(nums) - 1
+        n = len(nums)
+
+        for i in range(n):
+            # Skip duplicate values for the first position
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            # If the smallest value is positive, no way to get sum of 0
+            if nums[i] > 0:
+                break
+
+            left, right = i + 1, n - 1
             while left < right:
-                three_sum = num + nums[left] + nums[right]
-                if three_sum == 0:
-                    result.add((num, nums[left], nums[right]))
+                current_sum = nums[i] + nums[left] + nums[right]
+
+                if current_sum < 0:
                     left += 1
+                elif current_sum > 0:
                     right -= 1
-                elif three_sum < 0:
-                    left += 1
                 else:
+                    # Found a triplet
+                    results.append([nums[i], nums[left], nums[right]])
+
+                    # Skip duplicates
+                    while left < right and nums[left] == nums[left + 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    # Move both pointers inward
+                    left += 1
                     right -= 1
-        
-        return list(result)
+
+        return results
