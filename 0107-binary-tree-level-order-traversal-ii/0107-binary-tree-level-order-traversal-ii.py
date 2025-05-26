22d1df826a8c42842da82a7ef9ebd361327bb41f
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-05-26 22:30:29 +0530

    Time: 0 ms (100%), Space: 18 MB (58.53%) - LeetHub

--- 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,21 +7,37 @@
 #         self.right = right
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        """
+        Performs level-order traversal of a binary tree from bottom to top.
+
+        Args:
+            root: The root node of the binary tree
+        
+        Returns:
+            A list of lists containing node values level by level from bottom to top
+        """
         if not root:
             return []
-
-        result = []
-        queue = [root]
+        
+        queue = deque([root])
+        levels = []
 
         while queue:
-            level = []
-            for _ in range(len(queue)):
-                node = queue.pop(0)
-                level.append(node.val)
+            current_level = []
+            level_size = len(queue)
+
+            # Process all nodes at the current level
+            for _ in range(level_size):
+                node = queue.popleft()
+                current_level.append(node.val)
+                
+                # Add children to queue for next level
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            result.insert(0, level)
+
+            levels.append(current_level)
         
-        return result
+        # Reverse to get bottom-up order
+        return levels[::-1]
