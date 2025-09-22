167d5c2933af31fdf78996963aaf394afb2c8501
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-09-22 12:29:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.py
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.py
@@ -1,5 +1,4 @@
-#User function Template for python3
-
+from collections import deque
 
 '''
 # Node Class:
@@ -10,118 +9,35 @@ class Node:
         self.right = None
 '''
 
-#Function to return a list containing elements of left view of the binary tree.
 class Solution:
     def LeftView(self, root):
-        # If the tree is empty, return an empty list
+        """
+        Returns the left view of a binary tree using BFS approach.
+        
+        Time Complexity: O(n) where n is the number of nodes
+        Space Complexity: O(w) here w is the maximum width of the tree
+        """
         if not root:
             return []
         
-        left_view = [] # List to store the left view elements
+        result = []
+        queue = deque([root])
         
-        # Helper function to traverse the tree
-        def traverse(current, level):
-            if not current:
-                return
+        while queue:
+            level_size = len(queue)
             
-            # If this is the first node of its level
-            if level == len(left_view):
-                left_view.append(current.data)
-            
-            # Traverse the left and right subtree
-            traverse(current.left, level+1)
-            traverse(current.right, level+1)
+            # Process all nodes at the current level
+            for i in range(level_size):
+                node = queue.popleft()
+                
+                # The first node at each level is the leftmost
+                if i == 0:
+                    result.append(node.data)
+                
+                # Add children for next level
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
         
-        # Start the traversal from the root at level 0
-        traverse(root, 0)
-        return left_view
-
-#{ 
- # Driver Code Starts
-#Contributed by Sudarshan Sharma
-from collections import deque
-
-
-# Tree Node
-class Node:
-
-    def __init__(self, val):
-        self.right = None
-        self.data = val
-        self.left = None
-
-
-# Function to Build Tree
-def buildTree(s):
-    #Corner Case
-    if (len(s) == 0 or s[0] == "N"):
-        return None
-
-    # Creating list of strings from input
-    # string after spliting by space
-    ip = list(map(str, s.split()))
-
-    # Create the root of the tree
-    root = Node(int(ip[0]))
-    size = 0
-    q = deque()
-
-    # Push the root to the queue
-    q.append(root)
-    size = size + 1
-
-    # Starting from the second element
-    i = 1
-    while (size > 0 and i < len(ip)):
-        # Get and remove the front of the queue
-        currNode = q[0]
-        q.popleft()
-        size = size - 1
-
-        # Get the current node's value from the string
-        currVal = ip[i]
-
-        # If the left child is not null
-        if (currVal != "N"):
-
-            # Create the left child for the current node
-            currNode.left = Node(int(currVal))
-
-            # Push it to the queue
-            q.append(currNode.left)
-            size = size + 1
-        # For the right child
-        i = i + 1
-        if (i >= len(ip)):
-            break
-        currVal = ip[i]
-
-        # If the right child is not null
-        if (currVal != "N"):
-
-            # Create the right child for the current node
-            currNode.right = Node(int(currVal))
-
-            # Push it to the queue
-            q.append(currNode.right)
-            size = size + 1
-        i = i + 1
-    return root
-
-
-if __name__ == "__main__":
-    t = int(input())
-    for _ in range(0, t):
-        s = input()
-        root = buildTree(s)
-        ob = Solution()
-        result = ob.LeftView(root)
-        if len(result) == 0:
-            print("[]", end=" ")
-        for value in result:
-            print(value, end=" ")
-        print()
-
-        print("~")
-
-# } Driver Code Ends
+        return result
