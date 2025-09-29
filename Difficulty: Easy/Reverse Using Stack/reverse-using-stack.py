113027967123b98b9712d78827f3d7c75982ae6e
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-09-29 22:59:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Using Stack/reverse-using-stack.py
+++ Difficulty: Easy/Reverse Using Stack/reverse-using-stack.py
@@ -1,39 +1,7 @@
-#{ 
- # Driver Code Starts
-
-# } Driver Code Ends
-def reverse(S):
-    """
-    Reverse a string using a stack.
-    
-    Args:
-        string (str): Input string to be reversed
+class Solution:
+    def reverse(self, s: str) -> str:
+        if not s:
+            return ""
         
-    Returns:
-        str: Reversed string
-    
-    Example:
-        >>> reverse("hello")
-        'olleh'
-    """
-    # Early return for empty or single character strings
-    if len(S) <= 1:
-        return S
-    
-    stack = []
-    # Push phase
-    for char in S:
-        stack.append(char)
-    
-    # Pop phase - using list comprehension and join
-    return ''.join(stack.pop() for _ in range(len(stack)))
-
-#{ 
- # Driver Code Starts.
-if __name__=='__main__':
-    t=int(input())
-    for i in range(t):
-        str1=input()
-        print(reverse(str1))
-        print("~")
-# } Driver Code Ends
+        stack = list(s)  # directly initialize stack with characters
+        return ''.join(stack.pop() for _ in range(len(stack)))
