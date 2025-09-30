72cb190b82bbae67fd2380225d75962c131adcf0
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-09-30 21:19:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.py
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.py
@@ -1,26 +1,10 @@
-#User function Template for python3
-
 class Solution:    
-    #Function to return the count of number of elements in union of two arrays.
     def findUnion(self, a, b):
-        union_arr = set(a).union(set(b))
-        
-        return len(union_arr)
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-
-        a = [int(x) for x in input().strip().split()]
-        b = [int(x) for x in input().strip().split()]
-        ob = Solution()
-
-        print(ob.findUnion(a, b))
-        print("~")
-
-# } Driver Code Ends
+        """
+        Find union of two arrays (removes duplicates).
+        Time: O(n + m), Space: O(n + m)
+        """
+        # Create one set, update with other array
+        result = set(a)
+        result.update(b)
+        return sorted(result)
