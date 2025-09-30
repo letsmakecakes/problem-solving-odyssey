3fa7bb9d546d477e1850f6563ba43b487110b7ec
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-09-30 19:52:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (98.44%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,22 +5,25 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-            # Create a dummy node to serve as the starting point of the merged list
-            dummy = ListNode(0)
-            current = dummy
+        """
+        Merge two sorted linked lists iteratively.
+        Time: O(n + m), Space: O(1)
+        """
+        # Create dummy node to simply edge cases
+        dummy = ListNode(0)
+        current = dummy
 
-            # Traverse both lists and merge them
-            while list1 and list2:
-                if list1.val < list2.val:
-                    current.next = list1
-                    list1 = list1.next
-                else:
-                    current.next = list2
-                    list2 = list2.next
-                current = current.next
-            
-            # Attach the remaining nodes from either list1 or list2
-            current.next = list1 if list1 else list2
-
-            # Return the merged list, starting from the node after the dummy node.
-            return dummy.next
+        # Merge nodes while both lists have elements
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        
+        # Attach remaining nodes (at most one list is non-empty)
+        current.next = list1 if list1 else list2
+        
+        return dummy.next
