1dd768bf1006fff51aa929236272bc85f4e2f2ca
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-09-30 20:31:09 +0530

    Time: 6 ms (36.09%), Space: 17.8 MB (84.79%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,25 +5,27 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Add two numbers represented as linked lists (digits in reverse order).
+        Time: O(max(n, m)), Space: O(max(n, m))
+        """
+        dummy = ListNode(0)
+        current = dummy
         carry = 0
-        dummy_node = ListNode(0)
-        current = dummy_node
 
         while l1 or l2 or carry:
-            val_1 = l1.val if l1 else 0
-            val_2 = l2.val if l2 else 0
+            # Get values (0 if node is None)
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-            total = val_1 + val_2 + carry
-            carry = total // 10
-            insert_val = total % 10
+            total = val1 + val2 + carry
+            carry, digit = divmod(total, 10)
 
-            current.next = ListNode(insert_val)
+            current.next = ListNode(digit)
             current = current.next
 
-            if l1:
-                l1 = l1.next
-            
-            if l2:
-                l2 = l2.next
+            # Advance pointers in one line
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
         
-        return dummy_node.next
+        return dummy.next
