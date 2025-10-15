72a9db7a82932133784c542438f3fac2e234b792
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-10-15 22:59:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.py
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.py
@@ -3,59 +3,52 @@ from collections import deque
 '''
 class Node:
     def __init__(self, val):
-        self.right = None
         self.data = val
+        self.right = None
         self.left = None
 '''
 
 class Solution:
     def bottomView(self, root):
         """
-        Returns the bottom view of a binary tree using BFS and horizontal distance mapping.
+        Returns the bottom view of a binary tree using level-order traversal
+        with horizontal distance tracking.
         
-        The bottom view consists of nodes that are visible when the tree is viewed from below.
-        We use horizontal distance (HD) to track position relative to root:
-        - Root has HD = 0
-        - Left child has HD = parent_HD - 1  
-        - Right child has HD = parent_HD + 1
-        
-        Unlike top view, we keep updating nodes at each HD to get the bottommost node.
-        
-        Time Complexity: O(n) where n is the number of nodes
-        Space Complexity: O(n) for queue and hashmap storage
+        The bottom view consists of nodes visible when viewing the tree from below.
+        For each horizontal distance, the last node encountered during level-order
+        traversal is part of the bottom view.
         
         Args:
-            root: Root node of the binary tree
+            root: The root node of the binary tree
             
         Returns:
-            List of integers representing the bottom view from left to right
+            List of node values representing the bottom view from left to right
         """
+        # Handle edge case: empty tree
         if not root:
             return []
         
-        # Map to store the bottommost node at each horizontal distance
-        hd_map: Dict[int, Node] = {}
-        
-        # BFS queue storing (node, horizontal_distance) pairs
+        # Queue stores tuples of (node, horizontal_distance)
         queue = deque([(root, 0)])
-        min_hd = max_hd = 0
+        bottom_view = {}
+        min_hd = 0
+        max_hd = 0
         
         while queue:
-            node, horizontal_distance = queue.popleft()
-            
-            # Always update the node at this horizontal distance
-            # (later nodes in BFS will be at lower levels, hence "bottom")
-            hd_map[horizontal_distance] = node
+            node, hd = queue.popleft()
             
             # Track the range of horizontal distances
-            min_hd = min(min_hd, horizontal_distance)
-            max_hd = max(max_hd, horizontal_distance)
+            min_hd = min(hd, min_hd)
+            max_hd = max(hd, max_hd)
+            
+            # Update bottom view - last node at each horizontal distance wins
+            bottom_view[hd] = node.data
             
             # Add children with updated horizontal distances
             if node.left:
-                queue.append((node.left, horizontal_distance - 1))
+                queue.append((node.left, hd - 1))
             if node.right:
-                queue.append((node.right, horizontal_distance + 1))
+                queue.append((node.right, hd + 1))
         
-        # Build result using range from min to max horizontal distance
-        return [hd_map[hd].data for hd in range(min_hd, max_hd + 1)]
+        # Build result from left to right using horizontal distance range
+        return [bottom_view[hd] for hd in range(min_hd, max_hd + 1)]
