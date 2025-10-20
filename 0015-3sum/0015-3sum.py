30504dc90610b0dfa08a79651e0943b87d229849
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-10-20 23:17:36 +0530

    Time: 447 ms (88.49%), Space: 20.8 MB (19.97%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,38 +1,39 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        results = []
+        result = []
         nums.sort()
-        n = len(nums)
 
-        for i in range(n):
-            # Skip duplicate values for the first position
-            if i > 0 and nums[i] == nums[i-1]:
+        for i in range(len(nums) - 1):
+            # Skip duplicate values for the first element
+            if i > 0 and nums[i] == nums[i - 1]:
                 continue
             
-            # If the smallest value is positive, no way to get sum of 0
+            # Early termination: if smallest number is positive, no solution possible
             if nums[i] > 0:
                 break
 
-            left, right = i + 1, n - 1
+            left = i + 1
+            right = len(nums) - 1
+
             while left < right:
-                current_sum = nums[i] + nums[left] + nums[right]
+                three_sum = nums[i] + nums[left] + nums[right]
 
-                if current_sum < 0:
-                    left += 1
-                elif current_sum > 0:
-                    right -= 1
-                else:
-                    # Found a triplet
-                    results.append([nums[i], nums[left], nums[right]])
+                if three_sum == 0:
+                    result.append([nums[i], nums[left], nums[right]])
 
-                    # Skip duplicates
+                    # Skip duplicate values for left pointer
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
+                    
+                    # Skip duplicate values for right pointer
                     while left < right and nums[right] == nums[right - 1]:
                         right -= 1
                     
-                    # Move both pointers inward
                     left += 1
                     right -= 1
+                elif three_sum < 0:
+                    left += 1
+                else:
+                    right -= 1
 
-        return results
+        return result 
