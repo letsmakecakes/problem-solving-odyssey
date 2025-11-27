44754e833ffc13f1eed8df91a73d1b58f9ebd304
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-11-27 15:58:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.py
+++ Difficulty: Easy/Frequencies in a Limited Array/frequencies-in-a-limited-array.py
@@ -1,40 +1,7 @@
 class Solution:
-    # Function to count the frequency of all elements from 1 to N in the array.
     def frequencyCount(self, arr):
-        """
-        Counts frequency of elements from 1 to N in the array where N is array length.
-        
-        Args:
-            arr (List[int]): Input array of integers
-        
-        Returns:
-            List[int]: List containing frequencies of elements from 1 to N
-        """
-        n = len(arr)
-        # Initialize result array with zeros
-        frequencies = [0] * n
-        
-        # Count frequencies using array indexing
+        count_map = {}
         for num in arr:
-            if 1 <= num <= n: # Only count numbers in valid range
-                frequencies[num - 1] += 1
+            count_map[num] = count_map.get(num, 0) + 1
         
-        return frequencies
-
-
-
-#{ 
- # Driver Code Starts
-# Main code to read input and process each test case
-t = int(input())  # number of test cases
-for _ in range(t):
-    arr = list(map(int, input().split()))  # input array
-    s = Solution().frequencyCount(arr)  # find the frequencies
-
-    # Output formatting
-    if s:
-        print(" ".join(map(str, s)))  # Print the result
-    else:
-        print("[]")  # Print empty list if no valid frequencies
-
-# } Driver Code Ends
+        return [count_map.get(i, 0) for i in range(1, len(arr) + 1)]
