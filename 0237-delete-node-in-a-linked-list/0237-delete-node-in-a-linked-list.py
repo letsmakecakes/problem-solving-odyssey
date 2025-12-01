28196e7f3f646463a43aac7ab58976a95b613c91
Adwaith Rajeev <adwaithrajeev@gmail.com>
2025-12-01 23:30:02 +0530

    Time: 37 ms (80.29%), Space: 18.1 MB (26.51%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.py
@@ -7,13 +7,29 @@
 class Solution:
     def deleteNode(self, node):
         """
-        Delete a node from a singly-linked list given only acces to that node.
-        Note: The node to delete is guaranteed not to be tail node.
-
-        Strategy: Copy the next node's value and skip the next node.
+        Delete a node from a singly-linked list given only access to that node.
+        
+        This is a "trick" problem: we can't actually delete the node since we
+        don't have access to the previous node. Instead, we copy the next node's
+        value into the current node and skip over the next node.
+        
+        Constraints (guaranteed by problem):
+        - The node to be deleted is NOT the tail of the list
+        - The node to be deleted is a valid node in the list
+        
+        Args:
+            node: The node to be "deleted" (modified in-place)
+            
+        Returns:
+            None (modifies the linked list in-place)
+            
+        Example:
+            Input:  1 -> 2 -> 3 -> 4, node = 3
+            After:  1 -> 2 -> 4
+            (We copy 4's value to node 3, then skip node 4)
         """
-        # Copy next node's value to current node
+        # Copy the next node's value into current node
         node.val = node.next.val
-
-        # Skip the next node (effectively deleting it)
+        
+        # Skip over the next node (effectively "deleting" it)
         node.next = node.next.next
